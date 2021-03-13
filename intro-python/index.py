ed452392d6e65bf6d650d49e9dd515a16c0c7ef5
ervinsv92 <ervinsv92@gmail.com>
2021-03-12 23:10:34 -0600

    Hasta clase 23

---
+++ intro-python/index.py
@@ -0,0 +1,48 @@
+#esto es un comentario
+if 5 > 3:
+    #print("5 es mayor a 3")
+    var = 1
+
+x = 5
+y = "chanchito feliz"
+
+#print(x,y)
+
+a, b, c = 'la', 'le', 'li'
+#print(a,b,c)
+
+val1 = val2 = val3 = "nada"
+#print(val1, val2, val3)
+
+inicio = "hola "
+final = "mundo"
+
+#print(inicio+final)
+
+palabra = 'comilla simple' #string
+palabra2= "comillas dobles" #string
+
+entero = 20 #integer
+conDecimales = 20.2 #float
+complejo = 1j
+
+#print(palabra, palabra2, entero, conDecimales, complejo)
+
+lista = [1,2,3,3, "carro", "bici"]
+lista2 = lista.copy()
+lista.append(4)
+#lista.clear()
+#print(lista, lista2.count(3))
+#print(len(lista))
+#print(lista[0])
+
+#lista.pop()#elimina el ultimo elemento de la lista
+#print(lista)
+
+#lista.remove("carro") #elimina elemento por su valor
+#print(lista)
+
+lista.reverse()
+#lista.sort() #no deja ordenarla porque tiene tipos de datos diferentes
+print(lista)
+
